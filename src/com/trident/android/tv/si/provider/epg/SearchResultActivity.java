80551b2c36d497d3dad4488632c7eb0237d7533b
pierrchen <pierr.chen@gmail.com>
2011-07-01 15:29:41 +0800

    add a seperate SeachResultActivty and

--- src/com/trident/android/tv/si/provider/epg/SearchResultActivity.java
+++ src/com/trident/android/tv/si/provider/epg/SearchResultActivity.java
@@ -52,14 +52,13 @@ import android.view.View.OnClickListener;
  * 
  */
 
-public class EPGProviderActivity extends ListActivity {
+public class SearchResultActivity extends ListActivity {
 
-	private static final String TAG = "EPGProviderActivity";
+	private static final String TAG = "SearchResultActivity";
 
 	private TextView mStarTimeTextView;
 	private TextView mEndTimeTextView;
 
-	
 	private int mStartYear = 2011;
 	private int mStartMonth = 5; //start from 0
 	private int mStartDay = 1;
@@ -76,33 +75,7 @@ public class EPGProviderActivity extends ListActivity {
 	static final int DATE_DIALOG_ID = 0;
 	static final int END_DATE_DIALOG_ID = 1;
 
-	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-				int dayOfMonth) {
-			mStartYear = year;
-			mStartMonth = monthOfYear;
-			mStartDay = dayOfMonth;
-			start_time_utc = getUTCTime(mStartYear, mStartMonth, mStartDay);
-			updateTimeViewDisplay();
-			filterTheEventByTime();
-		}
-	};
-
-	private DatePickerDialog.OnDateSetListener mEndDateSetListener = new DatePickerDialog.OnDateSetListener() {
-
-		public void onDateSet(DatePicker view, int year, int monthOfYear,
-				int dayOfMonth) {
-			mEndYear = year;
-			mEndMonth = monthOfYear;
-			mEndDay = dayOfMonth;
-			end_time_utc = getUTCTime(mEndYear,mEndMonth,mEndDay);
-			updateTimeViewDisplay();
-			filterTheEventByTime();
-		}
-	};
-	
-	
+		
 	private long getUTCTime(int y, int m ,int d)
 	{
 		
@@ -115,87 +88,18 @@ public class EPGProviderActivity extends ListActivity {
 		
 	}
 
-	// use current startTime and endTime to filter the result
-	void filterTheEventByTime() {
-		
-//		//update the view 
-//		Intent myIntent = new Intent(EPGProviderActivity.this,
-//				EPGProviderActivity.class);
-//
-//		// The detailActivity will use this to query the detail
-//		// information.
-//		startActivity(myIntent);
-//		finish();
-		
-		String selection = " start_time > ? AND start_time < ? ";
-		Log.d(TAG, "search events between " + start_time_utc + "," + end_time_utc);
-		
-		String[] selectionArgs = {String.valueOf(start_time_utc), String.valueOf(end_time_utc)};
-		ListAdapter adapter = normalSearch( selection, selectionArgs, null);
-		setListAdapter(adapter);
-		
-		
-		
+	
 
-	}
-
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		switch (id) {
-		case DATE_DIALOG_ID:
-			return new DatePickerDialog(this, mDateSetListener, mStartYear,
-					mStartMonth, mStartDay);
-		case END_DATE_DIALOG_ID:
-			return new DatePickerDialog(this, mEndDateSetListener, mEndYear,
-					mEndMonth, mEndDay);
-		}
-		return null;
-	}
-
+	
 	/** Called when the activity is first created. */
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.search_result);
 
 		Log.d(TAG, "onCreate");
-		
-		mStarTimeTextView = (TextView) findViewById(R.id.startDateDisplay);
-		mEndTimeTextView = (TextView) findViewById(R.id.endDateDisplay);
-		
-		
-		
-	
-		mStarTimeTextView.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				showDialog(DATE_DIALOG_ID);
-			}
-		});
-
-
-		mEndTimeTextView.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				showDialog(END_DATE_DIALOG_ID);
-			}
-		});
-
-		// get the current date and init the start_time and end_time
-		
-//		final Calendar c = Calendar.getInstance();
-//		mStartYear = c.get(Calendar.YEAR);
-//		mStartMonth = c.get(Calendar.MONTH);
-//		mStartDay = c.get(Calendar.DAY_OF_MONTH);
-//
-//		mEndYear = c.get(Calendar.YEAR);
-//		mEndMonth = c.get(Calendar.MONTH);
-//		mEndDay = c.get(Calendar.DAY_OF_MONTH) + 1;
-
-		
-		// display the current date, which will be used as the search constraint
-		updateTimeViewDisplay();
-
-		
+				
 		ListView lv = getListView(); // == findViewById(android.R.id.list)
 
 		lv.setTextFilterEnabled(true);
@@ -205,9 +109,6 @@ public class EPGProviderActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-
-//				Intent myIntent = new Intent(EPGProviderActivity.this,
-//						EventDetail.class);
 				
 				Intent myIntent = new Intent("com.trident.tv.si.intent.action.PICK");
 				
@@ -220,123 +121,54 @@ public class EPGProviderActivity extends ListActivity {
 
 			}
 		});
-
-		// search button
-		Button searchButton = (Button) findViewById(R.id.searchbutton);
-
-		searchButton.setOnClickListener(new OnClickListener() {
+		
+		Button button = (Button)findViewById(R.id.backbutton2);
+		
+		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
-				// start an search
-				Log.d(TAG,"start search...............................");
-				onSearchRequested();
-				// finish() should not be called
-
-			}
-		});
-
-		// movie button
-		Button movieButton = (Button) findViewById(R.id.bt_movie);
-
-		movieButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				Log.d(TAG, "movie button has been pressed...........");
-				// start himself...
-//				Intent myIntent = new Intent(EPGProviderActivity.this,
-//						EPGProviderActivity.class);
 				
-				Intent myIntent = new Intent("com.trident.tv.si.intent.action.CATEGORY");
-
-				myIntent.putExtra("TYPE", "movie");
-				startActivity(myIntent);
+				startActivity(new Intent("com.trident.tv.si.intent.action.LIST"));
+				
 				finish();
 			}
 		});
-		
-		// news button
-		Button newsButton = (Button) findViewById(R.id.bt_news);
 
-		newsButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				Log.d(TAG, "news button has been pressed...........");
-				// start himself...
-//				Intent myIntent = new Intent(EPGProviderActivity.this,
-//						EPGProviderActivity.class);
-
-
-				Intent myIntent = new Intent("com.trident.tv.si.intent.action.CATEGORY");
-				myIntent.putExtra("TYPE", "news");
-				startActivity(myIntent);
-				finish();
-			}
-		});
-		
-		// movie button
-		Button sportsButton = (Button) findViewById(R.id.bt_sports);
-
-		sportsButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
-				Log.d(TAG, "sports button has been pressed...........");
-//				// start himself...
-//				Intent myIntent = new Intent(EPGProviderActivity.this,
-//						EPGProviderActivity.class);
-
-				Intent myIntent = new Intent("com.trident.tv.si.intent.action.CATEGORY");
-
-				myIntent.putExtra("TYPE", "sports");
-				startActivity(myIntent);
-				finish();
-			}
-		});
-		
-		//Handle the query ------->>>>>
+	   //Handle the query ------->>>>>
 		
 		Log.d(TAG,"Checking query type---------------------------");
 		
-		String[] query = getQueryType();
+		//String[] query = getQueryType();
 
 		ListAdapter adapter = null;
 		
-		Log.d(TAG,"Checking query type---------------------------" + query[0]);
+		String keyWords = getIntent().getStringExtra(SearchManager.QUERY);
+		
+		//Log.d(TAG,"Checking query type---------------------------" + query[0]);
 	
+		//adapter = searchByKeywords(keyWords);
+		
+		Cursor c = managedQuery(EPGProvider.CONTENT_URI_EVENTS_SEARCH,
+				new String[] { Events.SERVICE_ID, Events.NAME }, // selections
+				null, // always be NULL
+				new String[] { keyWords }, // the keywords
+				null);
+
+		int results = c.getCount();
+		
+		TextView textView =  (TextView)findViewById(R.id.search_result);
+		
+		textView.setText("Find " + results + " programs containing \"" + keyWords + "\"");
+		
+		adapter = getAdaptor(c);
 	
-		switch(Integer.valueOf(query[0]))
-		{
-			
-			case 2:adapter = searchByKeywords(query[1]);break;
-			case 3:adapter = searchByEventType(query[1]);break;
-			case 4:  //fall through
-			case 1: 
-			{
-				String selection = " start_time > ? AND start_time < ? ";
-				Log.d(TAG, "search events between " + start_time_utc + "," + end_time_utc);
-				
-				String[] selectionArgs = {String.valueOf(start_time_utc), String.valueOf(end_time_utc)};
-				
-				adapter = normalSearch( selection, selectionArgs,null);
-				
-				break;
-				}
-			default:
-			{
-				Log.d(TAG, "unexpect query type,  return ...");
-				return;
-			}
-				
-		}
 		
 		if(adapter == null)
 		{
 			Log.d(TAG, "Opooos. Find nothing....");
 			return; 
 		}
+		
 		setListAdapter(adapter);
 
 
@@ -370,17 +202,15 @@ public class EPGProviderActivity extends ListActivity {
 
 			return new String[] {"1", ""};
 		
-		}
-//		else if (Intent.ACTION_SEARCH.equals(intent.getAction())){
-//
-//			   // case 2 :intent from Search bar
-//				query = intent.getStringExtra(SearchManager.QUERY);
-//				Log.d(TAG, "Will search .." + query);
-//				
-//				return new String[] {"2", query};
-//
-//	    }
-		else if(getIntent().getAction().equals("com.trident.tv.si.intent.action.CATEGORY")) {
+		}else if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+
+			   // case 2 :intent from Search bar
+				query = intent.getStringExtra(SearchManager.QUERY);
+				Log.d(TAG, "Will search .." + query);
+				
+				return new String[] {"2", query};
+
+	    } else if(getIntent().getAction().equals("com.trident.tv.si.intent.action.CATEGORY")) {
 				//case 3:intent from movie/news button
 				Bundle bd = getIntent().getExtras();
 				
@@ -478,10 +308,10 @@ public class EPGProviderActivity extends ListActivity {
 
 		} else {
 			// When using FTS, the returned Cursor won't contain level column
-
+			//ID is must to be able get the detail information
 			adapter = new EventCursorAdaptor(this, R.layout.list_item, c,
-					new String[] { Events.SERVICE_ID, Events.NAME }, new int[] {
-							R.id.serviceID, R.id.eventName });
+					new String[] { Events.ID, Events.SERVICE_ID, Events.NAME }, 
+					new int[] { R.id._id, R.id.serviceID, R.id.eventName });
 
 		}
 
